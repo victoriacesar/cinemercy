95fac846959ab3bcce2530e51b88af6266c759c6
Victória César <victoriacesaras@gmail.com>
2025-02-11 16:54:51 -0300

    feat: add font poppins

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono, Inter } from 'next/font/google';
+import { Geist, Geist_Mono, Inter, Poppins } from 'next/font/google';
 import './globals.css';
 import ReactQueryProvider from '../providers/react-query-provider';
 
@@ -19,6 +19,13 @@ const inter = Inter({
   variable: '--font-inter',
 });
 
+const poppins = Poppins({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-poppins',
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+});
+
 export const metadata: Metadata = {
   title: 'Cine Mercy',
   description: 'For those who like to watch movies with friends',
@@ -32,7 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${inter.variable} antialiased ${poppins.variable}`}
       >
         <ReactQueryProvider>{children}</ReactQueryProvider>
       </body>
